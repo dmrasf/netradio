6864ed7a6e349d3f74773ed4a7ab6a9042d53698
dmrasf <dmrasf@qq.com>
2020-09-30 12:05:26 +0800

    server [30%]

--- src/server/medialib.c
+++ src/server/medialib.c
@@ -2,37 +2,74 @@
 #include "mytbf.h"
 #include "server_conf.h"
 #include <errno.h>
+#include <fcntl.h>
 #include <glob.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/syslog.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #define PATHSIZE 1024
+#define BITRATE (16 * 1024)
 
 struct channel_context_st {
     chnid_t chnid;
     char* desc;
     glob_t mp3glob;
     int fd;
-    off_t offset;
+    int pos;
     mytbf_t* mytbf;
 };
 
 static struct channel_context_st channel[MAXCHNID + 1];
 static int path2entry(char* path, int* num, struct mlib_listentry_st* ptr)
 {
-    glob_t globres;
+    glob_t descglob, mp3glob;
+    char descpath[PATHSIZE];
+    char mp3path[PATHSIZE];
+    int i;
+    FILE* fp;
+    int chnid = -1;
+    char* line;
+    size_t linesize;
 
+    snprintf(descpath, PATHSIZE, "%s/*.txt", path);
     // 对每个频道目录解析 掠过无效 保存到channel中
-    if (glob(path, 0, NULL, &globres)) {
-        return -1;
+    if (glob(descpath, 0, NULL, &descglob)) {
+        syslog(LOG_WARNING, "desc no match");
+        return 0;
+    }
+    snprintf(mp3path, PATHSIZE, "%s/*.mp3", path);
+    if (glob(mp3path, 0, NULL, &mp3glob)) {
+        syslog(LOG_WARNING, "mp3 no match");
+        return 0;
     }
 
-
-    ptr[*num].chnid = 1;
-    ptr[*num].desc = "edw";
+    // 解析描述文件
+    fp = fopen(descglob.gl_pathv[0], "r");
+    if (getline(&line, &linesize, fp) < 0) {
+        syslog(LOG_ERR, "getline ch num");
+        return -1;
+    }
+    chnid = atoi(line);
+    if (chnid < 1 || chnid > MAXCHNID) {
+        syslog(LOG_ERR, "chnid error");
+        return -1;
+    }
+    channel[chnid].chnid = chnid;
+    if (getline(&line, &linesize, fp) < 0) {
+        return -1;
+    }
+    channel[chnid].desc = line;
+    // 16Kb = 16*8Kbit = 128Kbit
+    channel[chnid].mytbf = mytbf_init(BITRATE, BITRATE * 5);
+    channel[chnid].mp3glob = mp3glob;
+    channel[chnid].pos = 0;
+    channel[chnid].fd = open(channel[chnid].mp3glob.gl_pathv[channel[chnid].pos], O_RDONLY);
+    ptr[*num].chnid = chnid;
+    ptr[*num].desc = line;
     (*num)++;
 
     return 0;
@@ -65,7 +102,6 @@ int mlib_getchnlist(struct mlib_listentry_st** list, int* list_size)
 
     for (i = 0; i < globres.gl_pathc; i++) {
         if (path2entry(globres.gl_pathv[i], &num, ptr) < 0) {
-            syslog(LOG_ERR, "path2entry()");
             exit(1);
         }
     }
@@ -79,29 +115,38 @@ int mlib_getchnlist(struct mlib_listentry_st** list, int* list_size)
 // 从制定频道读取数据
 ssize_t mild_readchn(chnid_t chnid, void* ptr, size_t n)
 {
-    int fd = channel[chnid].fd;
-
     size_t nleft;
     ssize_t nread;
+    int ret;
+    // n bitrate
+    ret = mytbf_fetchtoken(channel[chnid].mytbf, n);
 
-    nleft = n;
+    nleft = ret;
     while (nleft > 0) {
-        if ((nread = read(fd, ptr, nleft)) < 0) {
+        if ((nread = read(channel[chnid].fd, ptr, nleft)) < 0) {
             if (nleft == n)
                 return (-1); /* error, return -1 */
             else
                 break; /* error, return amount read so far */
         } else if (nread == 0) {
+            // 若读完一个文件，则打开下一个文件继续
+            close(channel[chnid].fd);
+            channel[chnid].pos = (channel[chnid].pos + 1) % channel[chnid].mp3glob.gl_pathc;
+            channel[chnid].fd = open(channel[chnid].mp3glob.gl_pathv[channel[chnid].pos], O_RDONLY);
             break; /* EOF */
         }
         nleft -= nread;
         ptr += nread;
     }
-    return (n - nleft); /* return >= 0 */
+    return (ret - nleft); /* return >= 0 */
 }
 
 int mlib_freechnlist(struct mlib_listentry_st* list)
 {
+    channel[list->chnid].chnid = -1;
+    close(channel[list->chnid].fd);
+    mytbf_destroy(channel[list->chnid].mytbf);
+    free(list->desc);
     free(list);
     return 0;
 }
